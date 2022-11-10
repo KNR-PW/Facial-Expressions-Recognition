77ec1fa2b0afd16417f3d01adea23c8f00775620
Adam Dąbkowski <77021122+AdamixD@users.noreply.github.com>
2022-11-10 16:10:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import tensorflow as tf
 import cv2
 import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# model_path = 'model_final.h5'
+model_path = 'model_final_extended.h5'
 
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-classifier = load_model('model_final_extended.h5')
+classifier = load_model(model_path)
 
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
-
-img_size = 128
 
 cap = cv2.VideoCapture(0)
 
@@ -17,6 +21,7 @@ while True:
     labels = []
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray, 1.32, 5)
+    img_size = 48 if model_path == 'model_final.h5' else 128
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (224, 224, 224), thickness=3)
@@ -25,7 +30,7 @@ while True:
 
         if np.sum([roi_gray]) != 0:
             roi = roi_gray.astype('float')
-            roi = tf.keras.utils.img_to_array(roi)
+            roi = img_to_array(roi)
             roi = np.expand_dims(roi, axis=0)
             prediction = classifier.predict(roi)[0]
             label = emotion_labels[prediction.argmax()]
@@ -33,7 +38,8 @@ while True:
             cv2.putText(frame, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (51, 51, 255), thickness=3)
         else:
             cv2.putText(frame, 'No Faces', (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (51, 51, 255), thickness=3)
-        cv2.imshow('Facial Expressions Detector', frame)
+
+    cv2.imshow('Facial Expressions Detector', frame)
 
     # To close the program, press q
     if cv2.waitKey(1) & 0xFF == ord('q'):
